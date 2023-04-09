167f6eaff69cf285d58cecc4e2533f70e07b01c0
He.Camille <camille.he@outlook.com>
2023-04-09 17:04:56 +0800

    feat: setup backend and provider aws

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "rf-state-lock-824709318323-ap-southeast-1"
+    key    = "terraform/state/cicd-pipeline-on-aws.json"
+    dynamodb_table = "tf-state-lock-dynamo"
+    region = "us-southeast-1"
+    profile = "default"
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.62.0"
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
+}
