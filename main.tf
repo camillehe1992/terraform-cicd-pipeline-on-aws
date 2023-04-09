76e4d473919430deb739f4d7b79b339266fdab6f
He.Camille <camille.he@outlook.com>
2023-04-09 18:19:43 +0800

    chore: format tf files

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   backend "s3" {
-    bucket = "rf-state-lock-824709318323-ap-southeast-1"
-    key    = "terraform/state/cicd-pipeline-on-aws.json"
+    bucket         = "rf-state-lock-824709318323-ap-southeast-1"
+    key            = "terraform/state/cicd-pipeline-on-aws.json"
     dynamodb_table = "tf-state-lock-dynamo"
-    region = "ap-southeast-1"
+    region         = "ap-southeast-1"
   }
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.62.0"
     }
   }
